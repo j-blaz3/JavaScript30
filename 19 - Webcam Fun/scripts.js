850003feab95b171ffba674bcc9a7a565904061d
j-blalock <justin.m.blalock@gmail.com>
2021-01-22 19:32:02 -0600

    19 - Webcam Fun v1

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,41 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo () {
+	navigator.mediaDevices.getUserMedia({video: true, audio: false })
+		.then(localMediaStream => {
+			console.log(localMediaStream);
+			video.srcObject = localMediaStream;
+			video.play();
+		})
+		.catch(err => {
+			console.error('Oh no...', err);
+		});
+}
+
+function paintToCanvas () {
+	const width = video.videoWidth;
+	const height = video.videoHeight;
+	canvas.width = width;
+	canvas.height = height;
+	// console.log(width, height);
+	
+	setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height);
+	}, 16);
+}
+
+function takePhoto() {
+	//played the sound
+	snap.currentTime = 0;
+	snap.play();
+
+	// take the data out of the canvas
+	const data = canvas.toDataURL('image/jpeg');
+	console.log(data);
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
